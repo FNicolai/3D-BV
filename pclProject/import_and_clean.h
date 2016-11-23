943860d53f85512a332beca91e7f9fd89283537f
Nicolas Limpert <nicolas.limpert@alumni.fh-aachen.de>
2016-11-23 17:21:19 +0100

    pclproject: CMake-Project for pcl handling
    This commit initially allows to read in a hard-coded pointcloud and
    write the number of cloud points to the console.

---
+++ pclProject/import_and_clean.h
@@ -0,0 +1,14 @@
+#ifndef IMPORT_AND_CLEAN_H
+#define IMPORT_AND_CLEAN_H
+
+
+class Import_And_Clean
+{
+public:
+    Import_And_Clean();
+    void start();
+private:
+
+};
+
+#endif // IMPORT_AND_CLEAN_H
---
+++ pclProject/main.cpp
@@ -0,0 +1,9 @@
+#include "import_and_clean.h"
+    
+int 
+main ()
+{
+    Import_And_Clean import_and_clean;
+    import_and_clean.start();
+    return 0;
+}
