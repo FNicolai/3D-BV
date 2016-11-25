d6a9a6f66156c17704c8584ccf87e4f12f29788c
FNicolai <fabian.nicolai@alumni.fh-aachen.de>
2016-11-25 17:13:09 +0100

    Converted planar_segmentation into a function
    Converted transform_to_origin into a function
    
    Implemented function to use a voxel-filter
    
    Implemented a function to segmentate in a better way. Called extract_indices()

--- pclProject/import_and_clean.cpp
+++ pclProject/import_and_clean.cpp
@@ -1,6 +1,8 @@
 #include "import_and_clean.h"
 
 #include <iostream>
+#include <string>
+
 #include <pcl/io/io.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/visualization/cloud_viewer.h>
@@ -14,6 +16,8 @@
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/sample_consensus/model_types.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/extract_indices.h>
 
 using namespace std;
 
@@ -131,7 +135,7 @@ void Import_And_Clean::start()
     std::cout << "Loaded "
               << visualizerCloud1->width * visualizerCloud1->height
               << " data points from the pcd with the following fields: "
-              << std::endl; 
+              << std::endl;
 
     //    for (size_t i = 0; i < cloud->points.size (); ++i)
     //        std::cout << "    " << cloud->points[i].x
@@ -151,74 +155,64 @@ void Import_And_Clean::start()
     //--------------------------
     // -----Vizualizer Init-----
     //--------------------------
+    int viewport0(0);
+    int viewport1(1);
     boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
-    viewer->setBackgroundColor(0.05, 0.05, 0.05, 0); // Setting background to a dark grey
+
+    /*
+     * xmax = 0.5 for second viewport (voxel_filter)
+     * xmax = 1.0 for normal screen
+     */
+    viewer->createViewPort(0.0, 0.0, 0.5, 1.0, viewport0);
+    viewer->setBackgroundColor(0.0, 0.0, 0.0, viewport0);
 
     pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> fixedCloud(visualizerCloud1);
-    viewer->addPointCloud<pcl::PointXYZRGB> (visualizerCloud1, fixedCloud, "fixedCloud");
+    viewer->addPointCloud<pcl::PointXYZRGB> (visualizerCloud1, fixedCloud, "fixedCloud", viewport0);
     //    viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_COLOR, 1.0f, 0.0f, 0.0f, "fixedCloud");
     //    viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "fixedCloud");
 
     pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> moveableCloud(visualizerCloud2);
-    viewer->addPointCloud<pcl::PointXYZRGB> (visualizerCloud2, moveableCloud, "moveableCloud");
+    viewer->addPointCloud<pcl::PointXYZRGB> (visualizerCloud2, moveableCloud,"moveableCloud", viewport0);
 
 
     //    viewer->addCoordinateSystem(2.0,"moveableCloud",0);
     //    viewer->setCameraPosition();
 
-    viewer->addCoordinateSystem(1.0, "fixedCloud", 0);
+    viewer->addCoordinateSystem(1.0, "fixedCloud", viewport0);
 
     viewer->initCameraParameters ();
-    int Axis_txt(0);
-    viewer->addText("Axis ?",10,15,"Axis",Axis_txt);
-    int Stepsize_txt(0);
-    viewer->addText("Stepsize: ?",10,30,"Stepsize",Stepsize_txt);
+    viewer->addText("Axis ?",10,15,"Axis", viewport0);
+    viewer->addText("Stepsize: ?",10,30,"Stepsize", viewport0);
 
     viewer->registerKeyboardCallback(keyBoardEventOccoured, (void*) &viewer);
 
+    /*
+     * Transform pcl to it's origin
+     * based on it's centroid
+     */
+    transform_to_origin(visualizerCloud1, viewer);
 
     /*
-     * Get center of pointcloud
+     * Get the max segmentation with planar segmentation
      */
-    Eigen::Vector4f centroid;
-    pcl::compute3DCentroid (*visualizerCloud1, centroid);
+    //    pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+    //    pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+    //    planar_segmentation(visualizerCloud1, coefficients, inliers);
+
 
     /*
-     * Get covariance matrix
+     * Do a voxel filterung to reduce points -> faster
      */
-    Eigen::Matrix3f covariance_matrix;
-    pcl::computeCovarianceMatrix (*visualizerCloud1, centroid, covariance_matrix);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr filtered_visualizerCloud1 (new pcl::PointCloud<pcl::PointXYZRGB>);
+    voxel_filter(visualizerCloud1,filtered_visualizerCloud1,viewer,viewport1);
 
     /*
-     * Move pointcloud to origin 0,0,0
+     * Do segmentation and get the largest segment
+     * More intelligent than planar_segmentation
+     *
+     * Needs viewport1 to be 0.5 in Xmax (see above)
      */
-    Eigen::Vector3f init_translationsVector(-centroid[0],-centroid[1],-centroid[2]);
-    Eigen::Vector3f init_rotationsVector(0,0,0);
-    Eigen::Affine3f init_transform;
-
-    init_transform = Eigen::Affine3f::Identity();
-    init_transform.pretranslate(init_translationsVector);
-    viewer->updatePointCloudPose("fixedCloud",init_transform);
-
-    /*
-     * Segmentation
-     */
-    pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
-    pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
-
-    // Create the segmentation object
-    pcl::SACSegmentation<pcl::PointXYZRGB> seg;
-
-    // Optional
-    seg.setOptimizeCoefficients (true);
-
-    // Mandatory
-    seg.setModelType (pcl::SACMODEL_PLANE);
-    seg.setMethodType (pcl::SAC_RANSAC);
-    seg.setDistanceThreshold (0.01);
-
-    seg.setInputCloud (visualizerCloud1);
-    seg.segment (*inliers, *coefficients);
+    extract_indices(filtered_visualizerCloud1);
 
     viewer->setCameraPosition(0,0,10,0,0,0,0,1,0);
     viewer->getRenderWindow()->GetRenderers()->GetFirstRenderer()->GetActiveCamera()->SetParallelProjection(1);
@@ -264,7 +258,150 @@ void Import_And_Clean::start()
         if(initialPositioningDone)
             break;
     }
+}
 
+void Import_And_Clean::transform_to_origin(pcl::PointCloud<pcl::PointXYZRGB>::Ptr &cloud_ptr_,
+                                           boost::shared_ptr<pcl::visualization::PCLVisualizer> &viewer_)
+{
+    /*
+     * Get center of pointcloud
+     */
+    Eigen::Vector4f centroid;
+    pcl::compute3DCentroid (*cloud_ptr_, centroid);
 
+    /*
+     * Move pointcloud to origin 0,0,0
+     */
+    Eigen::Vector3f init_translationsVector(-centroid[0],-centroid[1],-centroid[2]);
+    Eigen::Vector3f init_rotationsVector(0,0,0);
+    Eigen::Affine3f init_transform;
 
+    init_transform = Eigen::Affine3f::Identity();
+    init_transform.pretranslate(init_translationsVector);
+    viewer_->updatePointCloudPose("fixedCloud",init_transform);
+}
+
+void Import_And_Clean::planar_segmentation(pcl::PointCloud<pcl::PointXYZRGB>::Ptr &cloud_ptr_,
+                                           pcl::ModelCoefficients::Ptr &coefficients_,
+                                           pcl::PointIndices::Ptr &inliers_)
+{
+    /*
+     * Planar Segmentation
+     */
+
+    // Create the segmentation object
+    pcl::SACSegmentation<pcl::PointXYZRGB> seg;
+
+    // Optional
+    seg.setOptimizeCoefficients (true);
+
+    // Mandatory
+    seg.setModelType (pcl::SACMODEL_PLANE);
+    seg.setMethodType (pcl::SAC_RANSAC);
+    seg.setDistanceThreshold (0.10);
+
+    seg.setInputCloud (cloud_ptr_);
+    seg.segment (*inliers_, *coefficients_);
+
+    if (inliers_->indices.size () == 0)
+    {
+        PCL_ERROR ("Could not estimate a planar model for the given dataset.");
+        return;
+    }
+
+    for (size_t i = 0; i < inliers_->indices.size (); ++i)
+        std::cerr << inliers_->indices[i] << "    " << cloud_ptr_->points[inliers_->indices[i]].x << " "
+                  << cloud_ptr_->points[inliers_->indices[i]].y << " "
+                  << cloud_ptr_->points[inliers_->indices[i]].z << std::endl;
+
+    std::cerr << "Model coefficients: " << coefficients_->values[0] << " "
+              << coefficients_->values[1] << " "
+              << coefficients_->values[2] << " "
+              << coefficients_->values[3] << std::endl;
+
+    std::cerr << "Model inliers: " << inliers_->indices.size () << std::endl;
+
+    return;
+}
+
+void Import_And_Clean::voxel_filter(pcl::PointCloud<pcl::PointXYZRGB>::Ptr &cloud_ptr_in_,
+                                    pcl::PointCloud<pcl::PointXYZRGB>::Ptr &cloud_ptr_out_,
+                                    boost::shared_ptr<pcl::visualization::PCLVisualizer> &viewer_,
+                                    int viewport_){
+
+    std::cerr << "PointCloud before filtering: " << cloud_ptr_in_->width * cloud_ptr_in_->height << " data points." << std::endl;
+
+    // Create the filtering object: downsample the dataset using a leaf size of 1cm
+    pcl::VoxelGrid< pcl::PointXYZRGB > sor;
+    sor.setInputCloud (cloud_ptr_in_);
+    sor.setLeafSize (0.1f, 0.1f, 0.1f);
+    sor.filter (*cloud_ptr_out_);
+
+    std::cerr << "PointCloud after filtering: " << cloud_ptr_out_->width * cloud_ptr_out_->height << " data points." << std::endl;
+
+    viewer_->createViewPort (0.5, 0.0, 1.0, 1.0, viewport_);
+    viewer_->setBackgroundColor (0.3, 0.3, 0.3, viewport_);
+    std::stringstream ss;
+    ss << "Downsampled cloud: " << cloud_ptr_out_->width * cloud_ptr_out_->height << " data points";
+    viewer_->addText (ss.str() , 10, 10, "viewport1 text", viewport_);
+    pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> downsampledCloud(cloud_ptr_out_);
+    viewer_->addPointCloud<pcl::PointXYZRGB> (cloud_ptr_out_, downsampledCloud,"downsampledCloud", viewport_);
+}
+
+void Import_And_Clean::extract_indices(pcl::PointCloud<pcl::PointXYZRGB>::Ptr &filtered_cloud_ptr_)
+{
+
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_p (new pcl::PointCloud<pcl::PointXYZRGB>), cloud_f (new pcl::PointCloud<pcl::PointXYZRGB>);
+
+    // Write the downsampled version to disk
+//    pcl::PCDWriter writer;
+//    writer.write<pcl::PointXYZRGB> ("Scan1_downsampled.pcd", *filtered_cloud_ptr_, false);
+
+    pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients ());
+    pcl::PointIndices::Ptr inliers (new pcl::PointIndices ());
+    // Create the segmentation object
+    pcl::SACSegmentation<pcl::PointXYZRGB> seg;
+    // Optional
+    seg.setOptimizeCoefficients (true);
+    // Mandatory
+    seg.setModelType (pcl::SACMODEL_PLANE);
+    seg.setMethodType (pcl::SAC_RANSAC);
+    seg.setMaxIterations (1000);
+    seg.setDistanceThreshold (0.10);
+
+    // Create the filtering object
+    pcl::ExtractIndices<pcl::PointXYZRGB> extract;
+
+    int i = 0, nr_points = (int) filtered_cloud_ptr_->points.size ();
+    // While 30% of the original cloud is still there
+    while (filtered_cloud_ptr_->points.size () > 0.3 * nr_points)
+    {
+        // Segment the largest planar component from the remaining cloud
+        seg.setInputCloud (filtered_cloud_ptr_);
+        seg.segment (*inliers, *coefficients);
+        if (inliers->indices.size () == 0)
+        {
+            std::cerr << "Could not estimate a planar model for the given dataset." << std::endl;
+            break;
+        }
+
+        // Extract the inliers
+        extract.setInputCloud (filtered_cloud_ptr_);
+        extract.setIndices (inliers);
+        extract.setNegative (false);
+        extract.filter (*cloud_p);
+        std::cerr << "PointCloud representing the planar component: " << cloud_p->width * cloud_p->height << " data points." << std::endl;
+
+//        std::stringstream ss;
+//        ss << "segmented_plane_" << i << ".pcd";
+//        writer.write<pcl::PointXYZRGB> (ss.str (), *cloud_p, false);
+
+        // Create the filtering object
+        extract.setNegative (true);
+        extract.filter (*cloud_f);
+        filtered_cloud_ptr_.swap (cloud_f);
+        i++;
+    }
+
+    return;
 }
--- pclProject/import_and_clean.h
+++ pclProject/import_and_clean.h
@@ -1,12 +1,36 @@
 #ifndef IMPORT_AND_CLEAN_H
 #define IMPORT_AND_CLEAN_H
 
+#include <iostream>
+#include <pcl/io/io.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/visualization/keyboard_event.h>
+#include <vtkRenderWindow.h>
+#include <vtkCamera.h>
+#include <Eigen/Geometry>
+#include <pcl/common/centroid.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/point_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
 
 class Import_And_Clean
 {
 public:
     Import_And_Clean();
     void start();
+    void transform_to_origin(pcl::PointCloud<pcl::PointXYZRGB>::Ptr &cloud_ptr_,
+                             boost::shared_ptr<pcl::visualization::PCLVisualizer> &viewer_);
+    void planar_segmentation(pcl::PointCloud<pcl::PointXYZRGB>::Ptr &cloud_ptr_,
+                             pcl::ModelCoefficients::Ptr &coefficients_,
+                             pcl::PointIndices::Ptr &inliers_);
+    void voxel_filter(pcl::PointCloud<pcl::PointXYZRGB>::Ptr &cloud_ptr_in_,
+                      pcl::PointCloud<pcl::PointXYZRGB>::Ptr &cloud_ptr_out_,
+                      boost::shared_ptr<pcl::visualization::PCLVisualizer> &viewer_,
+                      int viewport_);
+    void extract_indices(pcl::PointCloud<pcl::PointXYZRGB>::Ptr &filtered_cloud_ptr_);
 private:
 
 };
